771b4e68ff73ed622562b30404c0498bff93060b
Gaurav kumar <gauravkumar13045@gmail.com>
2025-11-15 19:04:12 +0530

    ques1 completed

---
+++ Ques1.java
@@ -0,0 +1,15 @@
+
+
+
+public class Ques1 {
+    int age = 21;
+    String name = "Gaurav";
+    boolean Vote = true;
+    float height = 5.9f;
+
+    public static void main(String[] args) {
+        Ques1 obj = new Ques1();  
+        System.out.println(obj.age);
+       
+    }
+}
