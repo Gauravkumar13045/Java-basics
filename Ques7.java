d9b4d7ec3ce67900c735092a6a603546de4367ab
Gaurav kumar <gauravkumar13045@gmail.com>
2025-11-18 22:28:28 +0530

    ques8 completed

--- Ques7.java
+++ Ques7.java
@@ -1,9 +1,12 @@
 // Infinite loop question
 
 public class Ques7 {
-   public static void main(String[] args){
+   public static void main(String[] args) throws InterruptedException{
     for (;;) {
             System.out.println("Apna College");
+            Thread.sleep(500);
+
         }
    }
+   
 }
---
+++ Ques8.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class Ques8 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            while (true) {
+                System.out.println("Press 1 to Enter Marks");
+                Thread.sleep(1000);
+                System.out.println("Press 0 to Exit");
+                Thread.sleep(1000);
+
+                int input = sc.nextInt();
+
+                if (input == 1) {
+                    System.out.println("Enter the marks (out of 100): ");
+                    int marks = sc.nextInt();
+
+                    if (marks >= 90) {
+                        System.out.println("This is Good");
+                        Thread.sleep(1000);
+                        System.out.println("Because marks don’t matter but our effort does.");
+                    } else if (marks >= 60) {
+                        System.out.println("This is also Good");
+                        Thread.sleep(1000);
+                        System.out.println("Because marks don’t matter but our effort does.");
+                    } else if (marks >= 0) {
+                        System.out.println("This is Good as well");
+                        Thread.sleep(1000);
+                        System.out.println("Because marks don’t matter but our effort does.");
+                    } else {
+                        System.out.println("Invalid marks! Please enter between 0 and 100.");
+                    }
+
+                } else if (input == 0) {
+                    System.out.println("Stopped Successfully!!");
+                    break; 
+                } else {
+                    System.out.println("Invalid choice! Please press 1 or 0.");
+                }
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
