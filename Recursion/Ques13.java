325cec991b47d72841872ab69b7de841171d0fa8
Gaurav kumar <gauravkumar13045@gmail.com>
2025-11-24 18:15:01 +0530

    Ques13 complete

--- Recursion/Ques13.java
+++ Recursion/Ques13.java
@@ -1,5 +1,24 @@
 package Recursion;
 
 public class Ques13 {
-    
+    public static void main(String args[]) {
+        factor(-5, 1);
+
+    }
+
+    public static void factor(int n, int sum) {
+        if (n < 0) {
+            System.out.println("invalid number");
+            return;
+        }
+        if (n == 0) {
+            System.out.println(sum);
+            return;
+        }
+
+        sum *= n;
+        factor(n - 1, sum);
+
+    }
+
 }
